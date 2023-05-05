eb7936e5f5dab036fddba02cda4bfce5d395bc82
adi <adibush01@gmail.com>
2023-05-05 16:42:50 +0300

    dsda

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllItems } from './services/api';
+import normalizeItemName from './utils/itemUtils';
+import SignUp from './Login/SignUp';
+import MainPage from './components/MainPage';
+import GridContainer from './grid/GridContainer';
+import FavoritList from './favoriteList/FavoriteList';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Login/Login';
+import { AuthProvider } from './context/AuthProvider';
+
 
 function App() {
+
+  const [availableItems, setAvailableItems] = useState()
+
+  useEffect(() => {
+    getAllItems().then(
+      res => {
+        const items = res.data.map(item => {
+          const itemName = normalizeItemName(item.title);
+          return ({ ...item, displayName: itemName })
+        });
+
+        setAvailableItems(items)
+      }
+    );
+  }, []);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthProvider>
+        <MainPage />
+        <Routes>
+          <Route path="/" element={<GridContainer/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/favorite" element={<FavoritList/>} />
+        </Routes>
+      </AuthProvider>
+    </>
+
+
+
+
+
+    // <div >
+    //   {!availableItems ? "Loading..." :
+
+    //     <div>
+    //       <SignUp  items={availableItems} />
+    //     </div>
+    //   }
+
+    // </div>
   );
 }
 
